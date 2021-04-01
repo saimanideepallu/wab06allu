170b79697359d03bef1085d06cc22d5c1bd9b27e
saimanideepallu <manideepmlalgo@gmail.com>
2021-03-31 21:11:33 -0500

    Final commit with bonus

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # wab06allu
+Heroku Server: https://wab06allu.herokuapp.com/
+Class - Package
+class Package{
+    constructor(mode, price, quantity){
+        this.mode = mode;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
--- app.js
+++ app.js
@@ -6,6 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var packageRouter = require('./routes/package');
+var starsRouter = require('./routes/stars');
+var slotRouter = require('./routes/slot');
+
+
 
 var app = express();
 
@@ -21,6 +26,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/package', packageRouter);
+app.use('/stars', starsRouter);
+app.use('/slot', slotRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
